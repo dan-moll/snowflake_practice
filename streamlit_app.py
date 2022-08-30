16a90136efebf6ae235db7be95a27375c295f10c
Daniel Moll <danieljacobmoll@gmail.com>
2022-08-29 17:02:31 -0700

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.title("Parents new healthy dinner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
-streamlit.text('Kale, Spinach & Rocket Smoothie'🥗)
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔')
 streamlit.text('Avocado Toast🥑🍞')
